4428e9cc0cd07a78b9c9e5fe249ba72fa6e1313a
malvaaccesorios <malva.acces@gmail.com>
2025-09-18 11:30:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const observerOptions = {
         root: null,
         rootMargin: '0px',
-        threshold: 0.2 // El elemento es 20% visible
+        threshold: 0.15 // El elemento es 15% visible
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                entry.target.classList.add('is-visible');
-                observer.unobserve(entry.target);
+                const element = entry.target;
+                const animationType = element.dataset.animation;
+                
+                element.classList.add('animated', animationType, 'is-visible');
+                observer.unobserve(element);
             }
         });
     }, observerOptions);
 
-    const animatableElements = document.querySelectorAll('.animatable');
+    const animatableElements = document.querySelectorAll('[data-animation]');
     animatableElements.forEach(element => {
         observer.observe(element);
     });
